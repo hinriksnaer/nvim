55c940bf6b5d0a57c95016e0ab95667edd15c292
hinriksnaer <hgudmund@redhat.com>
2025-08-20 15:17:36 +0000

    updated git and harpoon bindings

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -13,14 +13,6 @@ return {
     'folke/which-key.nvim',
   },
 
-  -- runs at startup (before plugin loads)
-  init = function()
-    local ok, wk = pcall(require, 'which-key')
-    if ok then
-      wk.add { { '<leader>g', group = ' git' } }
-    end
-  end,
-
   -- let lazy.nvim create a lazy-loaded keymap
   keys = {
     { '<leader>gg', '<cmd>LazyGit<cr>', desc = ' LazyGit' },
